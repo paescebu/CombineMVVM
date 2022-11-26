1d95f6f7b19134c4b5ed69daaa8f6e918776d8a2
pascalburlet <pburlet@outlook.com>
2022-11-26 14:10:39 +0100

    initial commit of project

---
+++ Sources/CombineMVVM/ViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  ViewModel.swift
+//  
+//
+//  Created by Pascal Burlet on 15.11.22.
+//
+
+import Combine
+
+#warning("make remote package!")
+
+@MainActor
+public protocol ViewModel: ObservableObject where ObservableObjectPublisher == Self.ObjectWillChangePublisher {
+    var subscriptions: Set<AnyCancellable> { get set }
+    
+    func pipeUpdates<Observable: ObservableObject>(of observable: Observable) where ObservableObjectPublisher == Observable.ObjectWillChangePublisher
+    func pipeViewModelUpdates(of viewModel: some ViewModel)
+}
+
+public extension ViewModel where ObservableObjectPublisher == Self.ObjectWillChangePublisher {
+    func pipeUpdates<Observable: ObservableObject>(of observable: Observable) where ObservableObjectPublisher == Observable.ObjectWillChangePublisher {
+        observable.objectWillChange.sink { _ in
+            self.objectWillChange.send()
+        } receiveValue: { _ in }
+        .store(in: &subscriptions)
+    }
+    
+    func pipeViewModelUpdates(of viewModel: some ViewModel) {
+        pipeUpdates(of: viewModel)
+    }
+}
